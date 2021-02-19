2b0b4ff6c78fbf9d5ebc15c864a2d0377d3d78a7
BuildTools <oleg.revedzhuk@gmail.com>
2021-02-18 20:31:09 -0500

    Problem Definition

---
+++ Cpp Class Template Specialization/Cpp Class Template Specialization/Cpp Class Template Specialization.cpp
@@ -0,0 +1,8 @@
+/*
+You are given a main function which reads the enumeration values for two different types as input, then prints out the corresponding enumeration names.
+Write a class template that can provide the names of the enumeration values for both types. If the enumeration value is not valid, then print unknown.
+*/
+
+int main() {
+	return 0;
+}
